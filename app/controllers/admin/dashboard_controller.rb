2f206d7eeb83fddea2e34f08a511d725877d6ac6
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-03-04 23:47:52 +0000

    Updated admin dashboard to show counts for products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   
   def show
+    @products_count = Product.all().count(:name)
+    @categories_count = Category.all().count(:name)
   end
 end
