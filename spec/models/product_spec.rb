b3961eace7382358342216bb94175fd73c56eeb3
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-03-09 20:58:05 +0000

    Updated spec for Product to test validations

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  subject {
+    described_class.new(name: "Anything",
+                        price: 5.00,
+                        quantity: 3,
+                        category: Category.new(name: "Apparel"))
+  }
+
+  describe 'Validations' do
+    
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a price" do
+      subject.price_cents = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a quantity" do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a category" do
+      subject.category = nil
+      expect(subject).to_not be_valid
+    end
+
+  end
+end
